da805642d04071a73aa145213dfc350c4c75c137
A-r-v-i <babuaravindsv@gmail.com>
2020-06-18 22:35:30 +0530

    Cart animation

--- public/js/main.js
+++ public/js/main.js
@@ -14,3 +14,8 @@ function menuToggleClickHandler() {
 
 backdrop.addEventListener('click', backdropClickHandler);
 menuToggle.addEventListener('click', menuToggleClickHandler);
+
+function addToCart(){
+  const elem = document.createElement("div");
+  elem.classList.add("aroma");
+}
